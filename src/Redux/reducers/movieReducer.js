e6464b096f920e6253b0763550a8afc1c1aae8ab
David John <davidkjohn01@gmail.com>
2020-03-04 22:43:33 +0530

    :construction:

---
+++ src/Redux/reducers/movieReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  top_rated: null,
+  upcoming: null,
+  now_playing: null
+};
+
+export default (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
